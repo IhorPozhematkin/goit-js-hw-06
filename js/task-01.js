e9ffa7b3b5ea18cc04d1afd463f3ebea792f06d6
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-09-02 15:57:15 +0300

    02/09-1

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const numberCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${numberCategories.length}`);
+
+numberCategories.forEach(category => {
+    const title = category.firstElementChild.textContent;
+    const element = category.firstElementChild.nextElementSibling.children.length;
+console.log(`Category: ${title}`);
+console.log(`Elements: ${element}`)});
+
+
+
---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const addItem = function (array) {
+  return array.map(ingredient => {
+  const newItem = document.createElement("li");
+  newItem.textContent = ingredient;
+  newItem.classList.add("item");
+  return newItem;
+})};
+
+const list = document.querySelector("#ingredients");
+const items = addItem(ingredients);
+list.append(...items);
