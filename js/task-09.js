43c0c6828a4ad6ba84f40f7872d921d9ee5cfbc5
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-09-03 16:56:52 +0300

    03.09-1

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const span =document.querySelector('.color');
+button.addEventListener('click', changesColor);
+function changesColor(event) {
+const active = getRandomHexColor();
+span.textContent = active;
+document.body.style.backgroundColor = active;
+}
